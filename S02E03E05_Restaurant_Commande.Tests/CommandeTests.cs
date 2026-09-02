e0128c99c8be2c8ade586159a9457824ed0dedba
Alexandre Lachance <lachancea47@gmail.com>
2026-09-02 11:19:02 -0400

    E03 - Ajoute les tests sans dependance

---
+++ S02E03E05_Restaurant_Commande.Tests/CommandeTests.cs
@@ -0,0 +1,84 @@
+using Restaurant;
+namespace Restaurant.Tests;
+
+public class CommandeTests
+{
+    //[Fact]
+    //public void test()
+    //{
+    //    //ARANGE
+    //    //ACT
+    //    //ASSERT
+    //}
+
+    [Fact]
+    public void estVide_SousTotal0m_0Articles()
+    {
+        //ARANGE
+        Commande commande = new Commande("10");
+        //ACT
+        bool result = commande.EstVide;
+        //ASSERT
+        Assert.True(result);
+    }
+    [Fact]
+    public void sousTotal_3Article_TrenteDeuxEtDixDisieme()
+    {
+        //ARANGE
+        Commande commande = new Commande("10");
+        commande.AjouterLigne(new LigneCommande("C-10", "spag", 15, 3, 0));
+        decimal expected = 45;
+        //ACT
+        decimal result = commande.SousTotal;
+        //ASSERT
+        Assert.Equal(result, expected);
+    }
+    [Fact]
+    public void sousTotal_1ArticleAvecRabais_15()
+    {
+        //ARANGE
+        Commande commande = new Commande("10");
+        commande.AjouterLigne(new LigneCommande("C-10", "spag", 20, 1, 25));
+        decimal expected = 15;
+        //ACT
+        decimal result = commande.SousTotal;
+        //ASSERT
+        Assert.Equal(result, expected);
+    }
+    [Theory]
+    [InlineData("C-10","spag",-10,1,0,"prixUnitaire")]
+    [InlineData("C-10","spag",10,0,0,"quantite")]
+    [InlineData("C-10","spag",10,1,-10, "pourcentageRabais")]
+    [InlineData("C-10", "spag", 10, 1, 110, "pourcentageRabais")]
+    public void ajouteLigne_AvecUnArgumentInvalide_ArgumentOutOfRangeException(string codePlat,string descriptionPlat,decimal prixUnitaire, int quantite, decimal rabaix, string exceptionParamNameExpected)
+    {
+        // ARRANGE
+        Commande commande = new Commande("10");
+
+        // ACT et ASSERT
+        ArgumentOutOfRangeException exception = Assert.Throws<ArgumentOutOfRangeException>(() => new LigneCommande(codePlat,descriptionPlat,prixUnitaire,quantite,rabaix));
+        Assert.Equal(exceptionParamNameExpected, exception.ParamName);
+    }
+
+    [Fact]
+    public void ajouteLigne_LigneNUll_ArgumentException()
+    {
+        // ARRANGE
+        Commande commande = new Commande("10");
+        string expectedExceptionParamName = "codePlat";
+        // ACT et ASSERT
+        ArgumentException exception = Assert.Throws<ArgumentException>(() => commande.AjouterLigne(new LigneCommande("", "spag", 10, 1, 0)));
+        Assert.Equal(expectedExceptionParamName, exception.ParamName);
+    }
+
+    [Fact]
+    public void ajouteLigne_LigneNUll_ArgumentNullException()
+    {
+        // ARRANGE
+        Commande commande = new Commande("10");
+        string expectedExceptionParamName = "ligne";
+        // ACT et ASSERT
+        ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => commande.AjouterLigne(null));
+        Assert.Equal(expectedExceptionParamName, exception.ParamName);
+    }
+}
---
+++ S02E03E05_Restaurant_Commande.Tests/LigneCommandeTests.cs
@@ -0,0 +1,38 @@
+using Restaurant;
+namespace LigneCommandeTest;
+
+public sealed class LigneCommandeTests
+{
+    //[Fact]
+    //public void test()
+    //{
+    //    //ARANGE
+    //    //ACT
+    //    //ASSERT
+    //}
+    [Fact]
+    public void CalculerTotal_AvecAucunRabais_29m()
+    {
+        //ARANGE
+        LigneCommande ligneCommande = new LigneCommande("C-1042", "Spag", 14.50m, 2, 0m);
+        decimal expected = 29.00m;
+        //ACT
+        decimal prixFinal = ligneCommande.CalculerTotal();
+        //ASSERT
+        Assert.Equal(prixFinal, expected);
+    }
+    [Theory]
+    [InlineData("C-1042", "Spag", 10, 1, 0,10)]
+    [InlineData("C-1042", "Spag", 12.50, 2, 10,22.50)]
+    [InlineData("C-1042", "Spag", 3.25, 4, 20,10.40)]
+    [InlineData("C-1042", "Spag", 10, 2, 100,0)]
+    public void CalculerTotal_SansRabais_ProduitMontantNet(string code,string description,decimal prix,int quantite,decimal rabais,decimal expected)
+    {
+        //ARANGE
+        LigneCommande ligneCommande = new LigneCommande(code,description,prix,quantite,rabais);
+        //ACT
+        decimal prixFinal = ligneCommande.CalculerTotal();
+        //ASSERT
+        Assert.Equal(prixFinal,expected);
+    }
+}
